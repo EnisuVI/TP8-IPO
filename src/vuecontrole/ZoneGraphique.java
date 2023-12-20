2c9c47416e7806c079d127e87aab0d0632a8eb1f
Charly <66905063+EnisuVI@users.noreply.github.com>
2023-12-20 11:04:54 +0100

    Questions n°18, 19 et 20

--- src/vuecontrole/ZoneGraphique.java
+++ src/vuecontrole/ZoneGraphique.java
@@ -4,9 +4,10 @@ package vuecontrole;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
-public class ZoneGraphique extends JPanel implements MouseMotionListener {
+public class ZoneGraphique extends JPanel implements MouseMotionListener, MouseListener {
 
     private BarreBasse barreBasse;
     private BarreHaute barreHaute;
@@ -16,6 +17,7 @@ public class ZoneGraphique extends JPanel implements MouseMotionListener {
         else this.barreBasse = new BarreBasse();
         if(barreHaute != null) this.barreHaute = barreHaute;
         this.addMouseMotionListener(this);
+        this.addMouseListener(this);
         this.setBackground(Color.white);
     }
 
@@ -28,5 +30,30 @@ public class ZoneGraphique extends JPanel implements MouseMotionListener {
     public void mouseMoved(MouseEvent e) {
         System.out.println("Souris déplacée");
         this.barreBasse.deplacementSouris(e);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Bouton de la souris cliqué");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        System.out.println("Souris pressée sur un composant");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        System.out.println("Souris relachée");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        System.out.println("Souris entrée sur un composant");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        System.out.println("Souris sortie du composant");
     }
 }
