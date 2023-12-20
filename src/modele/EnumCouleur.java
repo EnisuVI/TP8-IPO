2e9db959f157ad108872c902751c1eab4aac78f8
Charly <66905063+EnisuVI@users.noreply.github.com>
2023-12-20 09:44:00 +0100

    Question n°13

---
+++ src/modele/EnumCouleur.java
@@ -0,0 +1,11 @@
+package modele;
+
+import java.awt.*;
+
+public enum EnumCouleur {
+    ROUGE (Color.red),
+    BLEU (Color.blue),
+    VERT (Color.green),
+    NOIR (Color.black),
+    JAUNE (Color.yellow);
+}
